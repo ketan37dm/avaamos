9ec3648f89e933c2dfc674d9f7bb4a7e42fd070f
Ketan Deshmukh <ketan37.dm@gmail.com>
2019-03-06 20:41:00 +0530

    added users_events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_150733) do
+ActiveRecord::Schema.define(version: 2019_03_06_151030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2019_03_06_150733) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users_events", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.boolean "rsvp"
+    t.string "status"
   end
 
 end
