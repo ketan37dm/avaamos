ff75cb1a58b3b00575c9d81e9e4c83d57f6700ed
ketan_deshmukh <ketan37.dm@gmail.com>
2019-03-31 13:55:16 +0530

    DRYed the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,61 @@
 require 'csv'
 
-# Populate Users
-user_creation_errors = {}
-count = 0
-CSV.foreach('db/seed_data/users.csv', headers: true) do |row|
-  begin
-    user = User.new(row.to_h)
-    user.save!
-    count += 1
-  rescue StandardError => e
-    user_creation_errors[row['username']] = e.message
-  end
-end
+USER_FILEPATH = 'db/seed_data/users.csv'
+USER_ERRORS_FILEPATH = 'db/seed_data/users_errors.csv'
+EVENT_FILEPATH = 'db/seed_data/events.csv'
+EVENT_ERRORS_FILEPATH = 'db/seed_data/event_errors.csv'
 
-puts "#{count} users created successfully"
-if user_creation_errors.present?
-  puts "Following are the errors"
-  puts user_creation_errors.inspect
-end
+class UserCreationError < StandardError; end
+class EventCreationError < StandardError; end 
+
+%w(user event).each do |entity|
+	define_method("#{entity}_errors_file")  do 
+		file_var = instance_eval("@#{entity}_errors_file")
+		return file_var if file_var
+		file = CSV.read(
+			instance_eval("#{entity.upcase}_FILEPATH"),
+			headers: true, 
+			return_headers: true
+		)
+		headers = file.headers + ['errors']
+		file_var ||= CSV.open(
+			instance_eval("#{entity.upcase}_ERRORS_FILEPATH"), 'w', headers: headers
+		)
+		file_var << headers
+		file_var
+	end 
+
+	define_method("populate_#{entity}") do 
+		errors_file = instance_eval("#{entity}_errors_file")
+		CSV.foreach(instance_eval("#{entity.upcase}_FILEPATH"), headers: true) do |row|
+			row = row.to_h
+		  begin
+		  	attrs = {}
+		  	if (entity == 'event')
+		  		attrs['title'] = row['title']
+		  		attrs['start_time'] = row['starttime']
+		  		attrs['end_time'] = row['endtime']
+		  		attrs['description'] = row['description']
+		  		attrs['rsvps'] = row['users#rsvp']
+		  		attrs['all_day'] = (row['allday'] == 'TRUE')
+		  	else 
+		  		attrs = row
+		  	end 
+		    object = "#{entity.classify}".constantize.new(attrs)
+		    if object.valid?
+		    	object.save!
+		    else
+		    	raise "#{entity.camelize}CreationError".constantize.new(object.errors.full_messages.join(', '))
+		    end
+		  rescue StandardError => e
+		  	row['errors'] = e.message
+		  	errors_file << row
+		  end
+		end
+		errors_file.close
+	end 
+end 
+
+populate_user
+populate_event
+
