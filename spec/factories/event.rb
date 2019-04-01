e8190c6eec4a2521d270c6c2e94ca3f03eac6698
ketan_deshmukh <ketan37.dm@gmail.com>
2019-04-01 12:03:19 +0530

    first spec added

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do 
   factory :event do 
-    title { 'sample' }
+    title { Faker::Name.name }
     start_time { Time.now - 5.hours }
     end_time { Time.now - 2.hours }
-    description { 'Some description' }
-    all_day { true }
+    description { Faker::Quote.famous_last_words }
+    all_day { false }
     rsvps { 'ketan#yes;chinmay#maybe' }
     completed { true }
   end 
--- spec/services/rsvp_resolver_service_spec.rb
+++ spec/services/rsvp_resolver_service_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 describe RsvpResolverService do 
   describe '#overlapping_events' do 
-    context 'when events are not overlapping' do 
-      
-
+    context 'when all_day is fale' do 
+      context 'when events are not overlapping' do 
+        let(:service) { RsvpResolverService.new }
+        let(:event1) { build(:event, start_time: Time.now - 10.hours, end_time: Time.now - 4.hours) }
+        let(:event2) { build(:event, start_time: Time.now - 3.hours, end_time: Time.now - 1.hour) }
+        it 'should return false' do
+          expect(service.send(:overlapping_events?, event1, event2)).to be_falsey
+        end 
+      end 
     end 
   end 
 end 
